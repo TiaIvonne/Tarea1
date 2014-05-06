edc3141ead16c470d10b0db5956db149782a1102
Ivonne Yáñez <ivonne.wftm@gmail.com>
2014-05-05 23:52:42 -0400

    Tareas java

---
+++ Nota.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Nota here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Nota
+{
+    public static void main(String[]args)
+    {
+        System.out.print("Ingrese nota");
+        double nota = Leer.datoDouble();
+        
+        if (nota == 10)
+        System.out.println("Matricula de honor");
+        else if (nota >= 9)
+        System.out.println("Sobresaliente");
+        else if (nota >= 7)
+        System.out.println("Notable");
+        else if (nota >=5)
+        System.out.println("Aprobado");
+        else 
+        System.out.println("Suspenso");
+
+    
+}
+}
